673ea1bd0414e856c0e6af356b458f5318797a36
Adrian Maseda <amaseda@gmail.com>
2015-11-10 14:41:11 -0500

    add view and routes solution to starter. no controller.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
     resources :songs
     resources :genres
   end
+
+  resources :songs do
+    member do
+      post 'add_favorite'
+      post 'remove_favorite'
+    end
+  end
 end
