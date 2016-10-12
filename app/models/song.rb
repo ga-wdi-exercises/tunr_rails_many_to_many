51d1f8c293a46cfeb181dec43d0f961fafdb730f
Nick Olds <nolds9@yahoo.com>
2016-10-12 00:11:15 -0400

    adds favorites solution

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,5 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+  has_many :favorites
+  has_many :users, through: :favorites
 end
---
+++ db/migrate/20161012040616_create_favorites.rb
@@ -0,0 +1,8 @@
+class CreateFavorites < ActiveRecord::Migration[5.0]
+  def change
+    create_table :favorites do |t|
+      t.references :user, foreign_key: true
+      t.references :song, foreign_key: true
+    end
+  end
+end
